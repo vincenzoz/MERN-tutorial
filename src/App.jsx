f30952925ed60474ec267b6c3bea0feceb9f1983
Vincenzo Zecchillo <vin.zecchillo@gmail.com>
2020-06-11 07:34:26 +0200

    passing data between issue table and issue row components

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,38 @@ class IssueAdd extends React.Component {
     return <div>ISSUE ADD</div>
   }
 }
+class IssueRow extends React.Component {
+  render() {
+    const style = this.props.rowStyle;
+    return (
+      <tr>
+        <td style={style}>{this.props.issueId}</td>
+        <td style={style}>{this.props.children}</td>
+      </tr>
+    )
+  }
+}
 class IssueTable extends React.Component {
   render() {
-    return <div>ISSUE TABLE</div>
+    const rowStyle = {border: "1px solid silver", padding: 4};
+    return (
+      <table style={{borderCollapse: "collapse"}}>
+        <thead>
+          <tr>
+            <th style={rowStyle}>ID</th>
+            <th style={rowStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <IssueRow rowStyle={rowStyle} issueId={1}>
+            Show user name
+          </IssueRow>
+          <IssueRow rowStyle={rowStyle} issueId={2}>
+            Add user nationality
+          </IssueRow>
+        </tbody>
+      </table>
+    )
   }
 }
 class IssueList extends React.Component {
