661bb4f9f4ba95e6eed96072425280dc9de1cd87
Vincenzo Zecchillo <vin.zecchillo@gmail.com>
2020-06-11 22:03:49 +0200

    added issues dynamically to the Issue row component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+const issueList = [
+  {
+  id:1, status: 'New', owner: 'Vincenzo', effort: 10,
+  created: new Date('2020-06-08'), due: undefined,
+  title: 'Display the user name on the home page'},
+  {id:2, status: 'Assigned', owner: 'Mark', effort: 5,
+  created: new Date('2020-06-01'), due: new Date('2020-06-05'),
+  title: 'Add user nationality'}
+];
 
-  class IssueFilter extends React.Component {
+class IssueFilter extends React.Component {
     render() {
       return <div>ISSUE FILTER</div>
     }
@@ -12,10 +21,16 @@ class IssueAdd extends React.Component {
 class IssueRow extends React.Component {
   render() {
     const style = this.props.rowStyle;
+    const issue = this.props.issue;
+
     return (
       <tr>
-        <td style={style}>{this.props.issueId}</td>
-        <td style={style}>{this.props.children}</td>
+        <td style={style}>{issue.id}</td>
+        <td style={style}>{issue.title}</td>
+        <td style={style}>{issue.created.toDateString()}</td>
+        <td style={style}>{issue.due ? issue.due.toDateString() : ''}</td>
+        <td style={style}>{issue.owner}</td>
+        <td style={style}>{issue.effort}</td>
       </tr>
     )
   }
@@ -23,21 +38,22 @@ class IssueRow extends React.Component {
 class IssueTable extends React.Component {
   render() {
     const rowStyle = {border: "1px solid silver", padding: 4};
+    const issueRows = issueList.map(issue => 
+    <IssueRow key={issue.id} rowStyle={rowStyle} issue={issue} />);
     return (
       <table style={{borderCollapse: "collapse"}}>
         <thead>
           <tr>
             <th style={rowStyle}>ID</th>
             <th style={rowStyle}>Title</th>
+            <th style={rowStyle}>Created</th>
+            <th style={rowStyle}>Due date</th>
+            <th style={rowStyle}>Author</th>
+            <th style={rowStyle}>Effort</th>
           </tr>
         </thead>
         <tbody>
-          <IssueRow rowStyle={rowStyle} issueId={1}>
-            Show user name
-          </IssueRow>
-          <IssueRow rowStyle={rowStyle} issueId={2}>
-            Add user nationality
-          </IssueRow>
+          {issueRows}
         </tbody>
       </table>
     )
