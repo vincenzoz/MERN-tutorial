bdb9c291254fafd3b6ea74430a0206843d5b3241
Vincenzo Zecchillo <vin.zecchillo@gmail.com>
2020-06-17 18:16:09 +0200

    added issue list API

--- server/server.js
+++ server/server.js
@@ -3,10 +3,24 @@ const {ApolloServer} = require('apollo-server-express');
 const fs = require('fs');
 
 let aboutMessage = "Issue Tracker  API v1.0";
+const issuesDB = [
+    {
+    id:1, status: 'New', owner: 'Vincenzo', effort: 10,
+    created: new Date('2020-06-08'), due: undefined,
+    title: 'Display the user name on the home page'
+    },
+    {
+    id:2, status: 'Assigned', owner: 'Mark', effort: 5,
+    created: new Date('2020-06-01'), due: new Date('2020-06-05'),
+    title: 'Add user nationality'
+    }
+];
+
 
 const resolvers = {
     Query: {
         about: () => aboutMessage,
+        issueList,
     },
     Mutation: {
         setAboutMessage,
@@ -15,6 +29,10 @@ const resolvers = {
 
 function setAboutMessage(_, {message}) {
     return aboutMessage = message;
+}
+
+function issueList() {
+    return issuesDB;
 }
 
 const server = new ApolloServer({
