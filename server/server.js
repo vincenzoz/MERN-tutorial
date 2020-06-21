31d331b3844a3d612b0ea136fc7c4a657f53b24e
Vincenzo Zecchillo <vin.zecchillo@gmail.com>
2020-06-21 19:59:23 +0200

    added issue create API

--- server/server.js
+++ server/server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const fs = require('fs');
 const {GraphQLScalarType} = require('graphql');
+const {Kind} = require('graphql/language');
 
 const GraphQLDate = new GraphQLScalarType({
     name: 'GraphQLDate',
     description: 'A Date() type in GraphQL as a scalar',
     serialize(value) {
         return value.toISOString();
+    },
+    parseValue(value) {
+        return new Date(value);
+    },
+    parseLiteral(ast) {
+        return (ast.kind == Kind.STRING) ? new Date(ast.value) : undefined;
     },
 });
 
@@ -33,12 +40,21 @@ const resolvers = {
     },
     Mutation: {
         setAboutMessage,
+        issueAdd,
     },
     GraphQLDate
 };
 
 function setAboutMessage(_, {message}) {
     return aboutMessage = message;
+}
+
+function issueAdd(_, {issue}) {
+    issue.created = new Date();
+    issue.id = issuesDB.length + 1;
+    if (issue.status == undefined) issue.status = 'New';
+    issuesDB.push(issue);
+    return issue;
 }
 
 function issueList() {
