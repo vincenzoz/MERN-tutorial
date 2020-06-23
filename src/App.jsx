81cdbedfb4685e340bf31663cec01d1b0a8bd5e6
Vincenzo Zecchillo <vin.zecchillo@gmail.com>
2020-06-23 22:19:57 +0200

    replaced issueAdd query definition using variables

--- src/App.jsx
+++ src/App.jsx
@@ -43,20 +43,16 @@ class IssueList extends React.Component {
 
   async createIssue(issue) {
     const query = `
-    mutation {
-      issueAdd( issue: {
-        title:"${issue.title}",
-        owner:"${issue.owner}",
-        due:"${issue.due.toISOString()}"
-      }) {
-        id
-      }
+    mutation issueAdd( $issue: IssueInputs!) {
+        issueAdd( issue: $issue) {
+          id
+        }
     }
     `;
     const response = await fetch('/graphql', {
       method: "POST",
       headers: {'Content-Type':'application/json'},
-      body: JSON.stringify({query})
+      body: JSON.stringify({query, variables: {issue}})
     });
 
     this.loadData();
