133a6898524dcb3c47f06a1ef15b102a88fb5d18
Vincenzo Zecchillo <vin.zecchillo@gmail.com>
2020-06-28 16:28:55 +0200

    added fetch wrap function and displayed errors

--- src/App.jsx
+++ src/App.jsx
@@ -30,15 +30,10 @@ class IssueList extends React.Component {
       }
     }`;
 
-    const response = await fetch('/graphql', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({query})
-    });
-
-    const body = await response.text();
-    const result = JSON.parse(body, jsonDateReviver)
-    this.setState({issueList: result.data.issueList});
+    const data = await this.graphQLFetch(query);
+    if (data) {
+      this.setState({issueList: data.issueList});
+    }
   }
 
   async createIssue(issue) {
@@ -49,15 +44,37 @@ class IssueList extends React.Component {
         }
     }
     `;
-    const response = await fetch('/graphql', {
-      method: "POST",
-      headers: {'Content-Type':'application/json'},
-      body: JSON.stringify({query, variables: {issue}})
-    });
-
-    this.loadData();
+    const data = await this.graphQLFetch(query, {issue});
+    if (data) {
+      this.loadData();
+    }
   }
 
+  async graphQLFetch(query, variables = {}) {
+    try {
+      const response = await fetch('/graphql', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({query, variables})
+      });
+      const body = await response.text();
+      const result = JSON.parse(body, jsonDateReviver);
+
+      if (result.errors) {
+        const error = result.errors[0];
+        if (error.extensions.code == 'BAD_USER_INPUT') {
+          const details = error.extensions.exception.errors.join('\n');
+          alert(`${error.message}: \n ${details}`);
+        } else {
+          alert(`${error.extensions.code}: \n ${error.message}`);
+        }
+      }
+      return result.data;
+    } catch (error) {
+      alert(`Error in sending data to server: ${error.message}`);
+    }
+    
+  }
   render() {
     return (
     <React.Fragment>
